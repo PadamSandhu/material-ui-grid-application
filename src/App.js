d4aa55424eecdaca1db0d70cf82a87426bb91c12
Padam Sandhu <padamsandhu@Padams-iMac.local>
2020-04-08 15:13:59 -0400

    added support for single-js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import {AppGrid} from './components/grid';
+import { AppGrid } from './components/grid';
 
-function App() {
-  return (
-    <div className="App">
-      <AppGrid/>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    hasError: false,
+  };
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    return this.state.hasError ? (
+      <div>Error</div>
+    ) : (
+      <div className="Grid-App">
+        <BrowserRouter>
+          <Route path="/" component={AppGrid} />
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;

ebecec345f2785c852358c7a2b79ee94040bc170
Padam Sandhu <padamsandhu@Padams-iMac.local>
2020-04-15 16:49:32 -0400

    updated store action

--- src/store/actions/fetchCartData.js
+++ src/store/actions/fetchCartData.js
@@ -1,10 +1,37 @@
+import axios from 'axios';
 import * as Types from './types';
 
-export const fetchChartData = () => (dispatch) => {
+const fetching = () => (dispatch) => {
   dispatch({
-    type: Types.FETCH_CHART_DATA,
-    payload: {
-      sample: [12, 2, 3, 4, 5, 6],
-    },
+    type: Types.FETCHING_CHART_DATA,
   });
+};
+
+const fetched = () => (dispatch) => {
+  dispatch({
+    type: Types.FETCHED_CHART_DATA,
+  });
+};
+
+const error = (payload) => (dispatch) => {
+  dispatch({
+    type: Types.ERROR_CHART_DATA,
+    payload,
+  });
+};
+
+export const fetchChartData = () => async (dispatch) => {
+  try {
+    dispatch(fetching());
+    const data = await axios.get(
+      'https://api.jsonbin.io/b/5e976f462940c704e1d8c750'
+    );
+    dispatch({
+      type: Types.FETCH_CHART_DATA,
+      payload: data.data,
+    });
+    dispatch(fetched());
+  } catch (data) {
+    dispatch(error(data));
+  }
 };

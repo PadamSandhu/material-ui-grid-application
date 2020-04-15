cf07bea2c1413c35573a8aa9c467114f165e743a
Padam Sandhu <padamsandhu@Padams-iMac.local>
2020-04-15 17:02:06 -0400

    Added API

--- src/containers/D3Graph/D3Graph.js
+++ src/containers/D3Graph/D3Graph.js
@@ -1,9 +1,31 @@
 import React, { useEffect } from 'react';
+import Skeleton from '@material-ui/lab/Skeleton';
+import { ForceReact } from '../../components/D3';
 
 export const D3Graph = ({ chartData, fetchChartData }) => {
   useEffect(() => {
     fetchChartData();
   }, []);
+  const width = 700;
+  const height = 500;
 
-  return <div>Hello</div>;
+  return (
+    <div
+      style={{
+        width: '50%',
+        margin: '0 auto',
+        marginTop: '50px',
+      }}
+    >
+      {!chartData.fetching && chartData.fetched ? (
+        <ForceReact
+          canvasWidth={width}
+          canvasHeight={height}
+          data={chartData.chartData}
+        />
+      ) : (
+        <Skeleton variant="rect" width={700} height={500} />
+      )}
+    </div>
+  );
 };

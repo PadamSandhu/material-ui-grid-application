aeab5fae1039cd2028325cdbaa589750b6c2b980
Padam Sandhu <padamsandhu@Padams-iMac.local>
2020-04-17 15:35:07 -0400

    Added react query

--- src/components/ReactQuery/reactQueryEx.js
+++ src/components/ReactQuery/reactQueryEx.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import QueryBuilder from 'react-querybuilder';
+import React, { useState } from 'react';
+
+import QueryBuilder, { formatQuery } from 'react-querybuilder';
 
 const fields = [
   { name: 'firstName', label: 'First Name' },
   { name: 'lastName', label: 'Last Name' },
   { name: 'age', label: 'Age' },
-  { name: 'address', label: 'Address' },
-  { name: 'phone', label: 'Phone' },
-  { name: 'email', label: 'Email' },
-  { name: 'twitter', label: 'Twitter' },
-  { name: 'isDev', label: 'Is a Developer?', value: false },
 ];
 
+const rules = {
+  id: 'g-535g_rGwUALImuI596NNO',
+  rules: [
+    {
+      id: 'r-OTu1FibS_5B7nVD3z2IZB',
+      field: 'firstName',
+      value: '',
+      operator: 'notNull',
+    },
+    {
+      id: 'r-YcBYRqUwB9V9Lm8e4Uze7',
+      field: 'lastName',
+      value: '',
+      operator: 'notNull',
+    },
+    {
+      id: 'r-DvOxPneXyv7ixBCL6OOK6',
+      field: 'age',
+      value: '18',
+      operator: '>=',
+    },
+  ],
+  combinator: 'and',
+  not: false,
+};
+
 export const ReactQueryEx = () => {
+  const [query, setQuery] = useState({ ...rules });
   return (
-    <>
+    <div
+      style={{
+        margin: '100px',
+      }}
+    >
+      <h2> Set Query Rules</h2>
       <QueryBuilder
         fields={fields}
-        onQueryChange={(query) => console.log(query)}
+        query={rules}
+        onQueryChange={(query) => setQuery(query)}
       />
-    </>
+
+      <div>
+        <h2> Create a Query based on Above Criteria </h2>
+        <pre>{formatQuery(query, 'json')}</pre>
+      </div>
+    </div>
   );
 };
